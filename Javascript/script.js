ea584989f524acb4c0515e83a3c3776f92de6f8e
Grant Warr <Grant Warr>
2017-02-01 11:14:53 -0400

    first upload

---
+++ Javascript/script.js
@@ -0,0 +1,91 @@
+function changeQ(){
+	var img = document.getElementById("keyq");
+	img.src  = "images/keyqon.png";
+}
+
+function changeW(){
+	var img = document.getElementById("keyw");
+	img.src  = "images/keywon.png";
+}
+
+function changeE(){
+	var img = document.getElementById("keye");
+	img.src  = "images/keyeon.png";
+}
+
+function changeR(){
+	var img = document.getElementById("keyr");
+	img.src  = "images/keyron.png";
+}
+
+function changeT(){
+	var img = document.getElementById("keyt");
+	img.src  = "images/keyton.png";
+}
+
+function changeY(){
+	var img = document.getElementById("keyy");
+	img.src  = "images/keyyon.png";
+}
+
+function changeQAgain(){
+	var img = document.getElementById("keyq");
+	img.src  = "images/keyq.png";
+}
+
+function changeWAgain(){
+	var img = document.getElementById("keyw");
+	img.src  = "images/keyw.png";
+}
+
+function changeEAgain(){
+	var img = document.getElementById("keye");
+	img.src  = "images/keye.png";
+}
+
+function changeRAgain(){
+	var img = document.getElementById("keyr");
+	img.src  = "images/keyr.png";
+}
+
+function changeTAgain(){
+	var img = document.getElementById("keyt");
+	img.src  = "images/keyt.png";
+}
+
+function changeYAgain(){
+	var img = document.getElementById("keyy");
+	img.src  = "images/keyy.png";
+}
+
+document.onkeydown = function(e) {
+	e = e || window.event;
+	if (e.keyCode == '81') 
+		changeQ();
+	if (e.keyCode == '87')
+		changeW();
+	if (e.keyCode == '69')
+		changeE();
+	if (e.keyCode == '82')
+		changeR();
+	if (e.keyCode == '84')
+		changeT();
+	if (e.keyCode == '89')
+		changeY();
+}
+
+document.onkeyup = function(e) {
+	e = e || window.event;
+	if (e.keyCode == '81') 
+		changeQAgain();
+	if (e.keyCode == '87')
+		changeWAgain();
+	if (e.keyCode == '69')
+		changeEAgain();
+	if (e.keyCode == '82')
+		changeRAgain();
+	if (e.keyCode == '84')
+		changeTAgain();
+	if (e.keyCode == '89')
+		changeYAgain();
+}
---
+++ README.txt
@@ -0,0 +1,7 @@
+README
+Thank you for viewing and using My Website.
+
+All images were taken from my phone camera (from my keyboard) with editing done with Adobe Photoshop CS5.
+
+myWebsite uses the Bootstap Framework for easy to make and responsive page styles. MLA Reference below:
+"Bootstrap v3.3.7". Otto, Mark, et al. 2011. Web. 31 Jan. 2017.
